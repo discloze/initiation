3fc409345e02b27f1ea905109a30bf878291343f
marlstergo <abdulmaliqshola@gmail.com>
2024-04-09 22:43:31 +0100

    remove react query integration

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,18 @@ import "@mantine/core/styles.css"
 import { MantineProvider } from "@mantine/core"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import LoginPage from "./components/pages/Login/LoginPage"
-import { QueryClient, QueryClientProvider } from "react-query"
 import { Dashboard } from "./components/pages/Dashboard/Dashboard"
-
-const queryClient = new QueryClient()
 
 function App() {
     return (
-        <QueryClientProvider client={queryClient}>
-            <MantineProvider>
-                <Router>
-                    <Routes>
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/" element={<Dashboard />} />
-                    </Routes>
-                </Router>
-            </MantineProvider>
-        </QueryClientProvider>
+        <MantineProvider>
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/" element={<Dashboard />} />
+                </Routes>
+            </Router>
+        </MantineProvider>
     )
 }
 
